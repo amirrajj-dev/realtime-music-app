074a31f5f848556f8f11a3cfca5961e571335575
amirrajj-dev <amiramraja@gmail.com>
2025-01-30 18:28:00 +0330

    added song routes and controllers🎵💜

---
+++ backend/src/controllers/song.controller.js
@@ -0,0 +1,69 @@
+import { songssModel } from "../models/song.model.js";
+
+const getRandomSongs = async (size, projection) => {
+  return await songssModel.aggregate([
+    {
+      $sample: { size },
+    },
+    {
+      $project: projection,
+    },
+  ]);
+};
+
+export const getSongs = async (req, res) => {
+  try {
+    const songs = await songssModel.find({}).sort({ _id: -1 });
+    res.status(200).json({ data: songs, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", success: false, error });
+  }
+};
+
+export const getFeaturedSongs = async (req, res) => {
+  try {
+    const projection = {
+      _id: 1,
+      title: 1,
+      artist: 1,
+      imageUrl: 1,
+      audioUrl: 1,
+    };
+    const songs = await getRandomSongs(6, projection);
+    res.status(200).json({ data: songs, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", success: false, error });
+  }
+};
+
+export const getMadeForYouSongs = async (req, res) => {
+  try {
+    const projection = {
+      _id: 1,
+      title: 1,
+      artist: 1,
+      imageUrl: 1,
+      audioUrl: 1,
+    };
+    const songs = await getRandomSongs(4, projection);
+    res.status(200).json({ data: songs, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", success: false, error });
+  }
+};
+
+export const getTrendingSongs = async (req, res) => {
+  try {
+    const projection = {
+      _id: 1,
+      title: 1,
+      artist: 1,
+      imageUrl: 1,
+      audioUrl: 1,
+    };
+    const songs = await getRandomSongs(4, projection);
+    res.status(200).json({ data: songs, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", success: false, error });
+  }
+};
--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,5 +1,11 @@
 import express from 'express'
-
+import {getSongs , getFeaturedSongs , getMadeForYouSongs , getTrendingSongs} from '../controllers/song.controller.js'
+import {adminProtectRoute , protectRoute} from '../middlewares/auth.middleware.js'
 const router = express.Router()
+
+router.get('/' , protectRoute , adminProtectRoute , getSongs)
+router.get('/featued-songs' , getFeaturedSongs)
+router.get('/made-for-you-songs' , getMadeForYouSongs)
+router.get('/tredits-songs' , getTrendingSongs)
 
 export default router;
