5e170708ca5e8eb4ef9423bd5226c82a52dc66c8
amirrajj-dev <amiramraja@gmail.com>
2025-01-30 15:05:22 +0330

    config cloudinary and added express file upload🎵💜

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,11 +7,23 @@ import songRoutes from './routes/song.route.js';
 import albumRoutes from './routes/album.route.js';
 import statRoutes from './routes/stat.route.js';
 import {connectToDb} from './utils/db.js'
+import fileUpload from 'express-fileupload'
+import path from 'path'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
 app.use(express.json())
+
+const dirname = path.resolve()
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(dirname , 'tmp'),  // path where files will be temporarily stored before moving them to the destination directory.
+    createParentPath: true,  // create parent directory if it doesn't exist.
+    limits: { fileSize: 15 * 1024 * 1024 },  // 15 MB file size limit.
+    abortOnLimit: true,  // abort the request if the file size exceeds the limit.
+    preserveExtension: true,  // keep original file extension.
+}))
 
 app.use('/api/users' , userRoutes)
 app.use('/api/auth' , authRoutes)
