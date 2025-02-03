0f287ce9da18332ceb6ec89ba5244674bc76fdb7
amirrajj-dev <amiramraja@gmail.com>
2025-02-03 17:06:30 +0330

    some neccesary changes in featured songs and music card🎵💜

--- frontend/src/components/FeaturedSongs.tsx
+++ frontend/src/components/FeaturedSongs.tsx
@@ -5,18 +5,18 @@ import MusicCardSkeleton from "./skeletons/MusicCardSkeleton";
 import { Typography } from "@mui/material";
 
 const FeaturedSongs: React.FC = () => {
-  const { featuredSongs, getFeaturedSongs , isLoading } = useMusicStore();
+  const { featuredSongs, getFeaturedSongs, isLoading } = useMusicStore();
 
   useEffect(() => {
-      getFeaturedSongs();
+    getFeaturedSongs();
   }, [getFeaturedSongs]);
 
   return (
-    <div>
-      <Typography className="mt-4 font-semibold" variant="h4">
-      Featured Songs 
+    <div className="flex flex-col items-center justify-center w-full">
+      <Typography className="mt-4 font-semibold text-center text-2xl md:text-4xl" variant="h4">
+        Featured Songs 🎧
       </Typography>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center items-center my-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full my-4">
         {isLoading
           ? Array.from(new Array(6)).map((_, index) => <MusicCardSkeleton key={index} />)
           : featuredSongs.map((song) => (
--- frontend/src/components/MusicCard.tsx
+++ frontend/src/components/MusicCard.tsx
@@ -9,10 +9,11 @@ interface MusicCardProps {
 }
 
 const MusicCard: React.FC<MusicCardProps> = ({ title, artist, imageUrl }) => {
-    const theme = useTheme();
-    const isDarkMode = theme.palette.mode === 'dark';
+  const theme = useTheme();
+  const isDarkMode = theme.palette.mode === 'dark';
+
   return (
-    <Card className="relative group w-full max-w-sm rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
+    <Card className="relative group w-full sm:max-w-sm rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
       <CardMedia
         component="img"
         alt={title}
@@ -20,7 +21,7 @@ const MusicCard: React.FC<MusicCardProps> = ({ title, artist, imageUrl }) => {
         className="w-full h-32 object-cover"
       />
       <IconButton
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity bg-emerald-500 rounded-full p-2"
         aria-label="play"
       >
         <PlayArrowIcon className={`${isDarkMode ? 'text-white' : 'text-indigo-700'}`} />
