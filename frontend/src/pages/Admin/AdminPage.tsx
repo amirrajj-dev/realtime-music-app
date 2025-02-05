ef388991f7e0417c858f0d6073c4d758d6b5c474
amirrajj-dev <amiramraja@gmail.com>
2025-02-05 19:03:32 +0330

    fetched necessary datas for admin page🎵💜

--- frontend/src/pages/Admin/AdminPage.tsx
+++ frontend/src/pages/Admin/AdminPage.tsx
@@ -1,8 +1,20 @@
 import { Box, Grid, useMediaQuery, useTheme } from "@mui/material";
+import { useStatsStore } from "../../store/useStats";
+import { useEffect } from "react";
+import { useMusicStore } from "../../store/useMusic";
+import { useAuthStore } from "../../store/useAuth";
 
 const AdminPage = () => {
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const {albumsCount , artistsCount , songsCount , usersCount , getStats} = useStatsStore()
+  const {albums , getAlbums , songs , getSongs} = useMusicStore()
+  const {user} = useAuthStore()
+  useEffect(()=>{
+    getStats()
+    getAlbums()
+    getSongs()
+  } , [getStats , getAlbums , getSongs])
   return (
     <Box
       sx={{
