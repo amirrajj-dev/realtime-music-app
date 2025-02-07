240e9482663dee4eac316d3f940507c17a554d5c
amirrajj-dev <amiramraja@gmail.com>
2025-02-08 01:39:22 +0330

    handle the route for getting the messages between two specific users🎵💜

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,34 +1,70 @@
 import { usersModel } from "../models/user.model.js";
 
-export const getUsers = async (req, res) =>{
-    try {
-        const currentUser = req.user
-        //get all users except the current user
-        const users = await usersModel.find({_id : {$ne : currentUser._id}} , 'email , fullname')
-        
-        res.json({ data : users, success: true });
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error", success: false, error });
-    }
-}
+export const getUsers = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    //get all users except the current user
+    const users = await usersModel.find(
+      { _id: { $ne: currentUser._id } },
+      "email , fullname"
+    );
 
-export const getUser = async (req, res) =>{
-    try {
-        const user = req.user
-        const currentUser = {
-            fullname : user.fullname,
-            email : user.email,
-            id : user._id,
-            createdAt : user.createdAt
-        }
-        if (!user){
-            return res.status(401).json({ message: "Unauthorized", success: false });
-        }
-        return res.status(200).json({
-            data: currentUser,
-            success: true
-        })
-    } catch (error) {
-        res.status(500).json({ message: "Internal server error", success: false, error})
+    res.json({ data: users, success: true });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", success: false, error });
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const user = req.user;
+    const currentUser = {
+      fullname: user.fullname,
+      email: user.email,
+      id: user._id,
+      createdAt: user.createdAt,
+    };
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized", success: false });
     }
-}
+    return res.status(200).json({
+      data: currentUser,
+      success: true,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", success: false, error });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const currentUser = req.user;
+    const { userId } = req.params;
+    const user = await usersModel.findById(userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    const messages = await messagesModel.find({
+      $or: [
+        { senderId: currentUser._id, receiverId: userId },
+        { senderId: userId, receiver: currentUser._id },
+      ],
+    }).sort({_id : -1})
+
+    return {
+        data : messages,
+        success : true
+    }
+
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", success: false, error });
+  }
+};
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { protectRoute } from '../middlewares/auth.middleware.js';
-import { getUsers , getUser } from '../controllers/user.controller.js';
+import { getUsers , getUser , getMessages } from '../controllers/user.controller.js';
 
 const router = express.Router()
 
 router.use(protectRoute)
 router.get('/' , getUsers)
 router.get('/curentuser' , getUser)
+router.get('/messages/:userId' , getMessages)
 
 export default router;
